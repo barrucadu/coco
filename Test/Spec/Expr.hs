89424d80e3439d582b0ce69dd76d03c331c01f00
Michael Walker <mike@barrucadu.co.uk>
2017-01-18 20:07:39 +0000

    Implement my own Typeable and Dynamic, which work with monads.

--- Test/Spec/Expr.hs
+++ Test/Spec/Expr.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 
 -- |
 -- Module      : Test.Spec.Expr
@@ -7,52 +8,47 @@
 -- License     : MIT
 -- Maintainer  : Michael Walker <mike@barrucadu.co.uk>
 -- Stability   : experimental
--- Portability : GADTs, ScopedTypeVariables
+-- Portability : GADTs, ScopedTypeVariables, TypeApplications
 --
 -- Constructing and evaluating dynamically-typed monadic expressions.
 --
 -- @
 -- > :{
--- runST $ do
---   let stateplus = statefun1 "s+" (\v a -> modifySTRef v (+a))
---   var <- newSTRef (5::Int)
---   case evaluate var =<< (stateplus $$ five) of
---     Just act -> act
---     Nothing  -> pure ()
---   readSTRef var
+-- let ioplus = constant @(MVar Int) @IO @(MVar Int -> Int -> IO ()) "io+" (\v a -> modifyMVar_ v (\x -> pure $ x+a))
+-- let five   = showConstant (5::Int)
+-- mvar <- newMVar (5::Int)
+-- case evaluate @(MVar Int) @IO @(IO ()) mvar . fromJust $ fromJust (ioplus $$ stateVariable) $$ five of
+--   Just act -> act
+--   Nothing  -> pure ()
+-- readMVar mvar
 -- :}
 -- 10
 -- @
+--
+-- As you can see, there is basically no type inference working at the
+-- moment. I hope that this will improve as I build more on top.
 module Test.Spec.Expr
   ( -- * Expressions
     Expr
   , constant
   , showConstant
-  , statefun0
-  , statefun1
-  , statefun2
-  , statefun3
   , variable
+  , stateVariable
   , ($$)
   , constants
   , variables
   , assign
   , evaluate
+  , evaluateDyn
 
   -- * Types
   , exprTypeArity
   , exprTypeRep
-  , stateTypeRep
-  , monadTypeRep
-  , stateTyCon
-  , monadTyCon
   ) where
 
 import Data.Char (isAlphaNum)
-import Data.Dynamic (dynApply, fromDynamic, toDyn)
-import Data.List (intercalate, nub)
-import Data.Proxy (Proxy(..))
-import Data.Typeable (Typeable, TypeRep, TyCon, gcast, funResultTy, mkTyCon3, mkTyConApp, typeOf, typeRep)
+import Data.Function (on)
+import Data.List (intercalate, nub, nubBy)
 
 import Test.Spec.Type
 
@@ -63,33 +59,21 @@ import Test.Spec.Type
 data Expr s m where
   -- It's important that these constructors aren't exposed, so the
   -- correctness of the 'TypeRep's can be ensured.
-  Constant  :: Typeable a => String -> a -> TypeRep -> Expr s m
-  Variable  :: String -> TypeRep -> Expr s m
-  StateFun0 :: Typeable a => String -> (s -> m a) -> TypeRep -> Expr s m
-  StateFun1 :: (Typeable a, Typeable b) => String -> (s -> a -> m b) -> TypeRep -> Expr s m
-  StateFun2 :: (Typeable a, Typeable b, Typeable c) => String -> (s -> a -> b -> m c) -> TypeRep -> Expr s m
-  StateFun3 :: (Typeable a, Typeable b, Typeable c, Typeable d) => String -> (s -> a -> b -> c -> m d) -> TypeRep -> Expr s m
-  FunAp     :: Expr s m -> Expr s m -> TypeRep -> Expr s m
+  Constant :: String -> Dynamic s m -> Expr s m
+  Variable :: String -> TypeRep s m -> Expr s m
+  StateVar :: Expr s m
+  FunAp    :: Expr s m -> Expr s m -> TypeRep s m -> Expr s m
 
 instance Show (Expr s m) where
   show = go True where
-    go _ (Constant s _ _) = toPrefix s
+    go _ (Constant s _) = toPrefix s
     go _ (Variable s _) = toPrefix s
-    go _ (StateFun0 s _ _) = toPrefix s
-    go _ (StateFun1 s _ _) = toPrefix s
-    go _ (StateFun2 s _ _) = toPrefix s
-    go _ (StateFun3 s _ _) = toPrefix s
+    go _ StateVar = ":state:"
     go b ap =
       let inner = intercalate " " $ case unfoldAp ap of
-            [(Constant s _ _), arg1, arg2]
+            [(Constant s _), arg1, arg2]
               | isSymbolic s -> [go False arg1, s, go False arg2]
             [(Variable s _), arg1, arg2]
-              | isSymbolic s -> [go False arg1, s, go False arg2]
-            [(StateFun1 s _ _), arg1, arg2]
-              | isSymbolic s -> [go False arg1, s, go False arg2]
-            [(StateFun2 s _ _), arg1, arg2]
-              | isSymbolic s -> [go False arg1, s, go False arg2]
-            [(StateFun3 s _ _), arg1, arg2]
               | isSymbolic s -> [go False arg1, s, go False arg2]
             unfolded -> map (go False) unfolded
       in if b then inner else "(" ++ inner ++ ")"
@@ -104,46 +88,34 @@ instance Show (Expr s m) where
     isSymbolic = not . all isAlphaNum
 
 -- | A constant value.
-constant :: Typeable a => String -> a -> Expr s m
-constant s a = Constant s a (typeOf a)
+constant :: HasTypeRep s m a => String -> a -> Expr s m
+constant s a = Constant s (toDyn a)
 
 -- | A constant value from a type which can be shown.
-showConstant :: (Show a, Typeable a) => a -> Expr s m
+showConstant :: (Show a, HasTypeRep s m a) => a -> Expr s m
 showConstant a = constant (show a) a
 
--- | A monadic function on the state.
-statefun0 :: forall s m a. Typeable a => String -> (s -> m a) -> Expr s m
-statefun0 s f = StateFun0 s f $ appFunTys stateTypeRep [monadTypeRep $ typeRep (Proxy :: Proxy a)]
-
--- | A monadic function on the state taking 1 other argument.
-statefun1 :: forall s m a b. (Typeable a, Typeable b) => String -> (s -> a -> m b) -> Expr s m
-statefun1 s f = StateFun1 s f $ appFunTys stateTypeRep [typeRep (Proxy :: Proxy a), monadTypeRep $ typeRep (Proxy :: Proxy b)]
-
--- | A monadic function on the state taking 2 other arguments.
-statefun2 :: forall s m a b c. (Typeable a, Typeable b, Typeable c) => String -> (s -> a -> b -> m c) -> Expr s m
-statefun2 s f = StateFun2 s f $ appFunTys stateTypeRep [typeRep (Proxy :: Proxy a), typeRep (Proxy :: Proxy b), monadTypeRep $ typeRep (Proxy :: Proxy c)]
-
--- | A monadic function on the state taking 3 other arguments.
-statefun3 :: forall s m a b c d. (Typeable a, Typeable b, Typeable c, Typeable d) => String -> (s -> a -> b -> c -> m d) -> Expr s m
-statefun3 s f = StateFun3 s f $ appFunTys stateTypeRep [typeRep (Proxy :: Proxy a), typeRep (Proxy :: Proxy b), typeRep (Proxy :: Proxy c), monadTypeRep $ typeRep (Proxy :: Proxy d)]
-
 -- | A variable.
-variable :: Typeable a => String -> proxy a -> Expr s m
+variable :: HasTypeRep s m a => String -> proxy a -> Expr s m
 variable s = Variable s . typeRep
+
+-- | The state variable
+stateVariable :: Expr s m
+stateVariable = StateVar
 
 -- | Apply a function, if well-typed.
 ($$) :: Expr s m -> Expr s m -> Maybe (Expr s m)
 f $$ e = FunAp f e <$> (exprTypeRep f `funResultTy` exprTypeRep e)
 
 -- | Get all constants in an expression, without repetition.
-constants :: Expr s m  -> [(String, TypeRep)]
-constants = nub . go where
-  go (Constant s _ ty) = [(s, ty)]
+constants :: Expr s m -> [(String, Dynamic s m)]
+constants = nubBy ((==) `on` fst) . go where
+  go (Constant s dyn) = [(s, dyn)]
   go (FunAp f e _) = go f ++ go e
   go _ = []
 
 -- | Get all variables in an expression, without repetition.
-variables :: Expr s m -> [(String, TypeRep)]
+variables :: Expr s m -> [(String, TypeRep s m)]
 variables = nub . go where
   go (Variable s ty) = [(s, ty)]
   go (FunAp f e _) = variables f ++ variables e
@@ -164,39 +136,25 @@ assign s v e@(Variable s2 ty)
 assign s v (FunAp f e ty) = FunAp <$> assign s v f <*> assign s v e <*> Just ty
 assign _ _ e = Just e
 
--- | Evaluate an expression, if it has no free variables, all state
--- function applications are fully saturated, and it is the correct
--- type.
-evaluate :: forall s m a. (Monad m, Typeable a) => s -> Expr s m -> Maybe (m a)
-evaluate s = go where
-  go :: forall b. Typeable b => Expr s m -> Maybe (m b)
-  go (Constant _ a _) = gcast (pure a)
-  go (StateFun0 _ f _) = gcast (f s)
-  go (FunAp (StateFun1 _ f _) a _) = do
-    a' <- go a
-    gcast $ do { a'' <- a'; f s a'' }
-  go (FunAp (FunAp (StateFun2 _ f _) a _) b _) = do
-    a' <- go a
-    b' <- go b
-    gcast $ do { a'' <- a'; b'' <- b'; f s a'' b'' }
-  go (FunAp (FunAp (FunAp (StateFun3 _ f _) a _) b _) c _) = do
-    a' <- go a
-    b' <- go b
-    c' <- go c
-    gcast $ do { a'' <- a'; b'' <- b'; c'' <- c'; f s a'' b'' c'' }
+-- | Evaluate an expression, if it has no free variables and it is the
+-- correct type.
+evaluate :: forall s m a. (Monad m, HasTypeRep s m a) => s -> Expr s m -> Maybe a
+evaluate s e = fromDyn @s @m @a =<< evaluateDyn s e
+
+-- | Evaluate an expression, if it has no free variables.
+evaluateDyn :: forall s m. Monad m => s -> Expr s m -> Maybe (Dynamic s m)
+evaluateDyn s = go where
+  go StateVar = Just (toDyn @s @m @s s)
+  go (Constant _ dyn) = Just dyn
+  go (Variable _ _) = Nothing
   go (FunAp f e _) = do
-    f' <- dyngo f
-    e' <- dyngo e
-    fmap pure . fromDynamic =<< dynApply f' e'
-  go _ = Nothing
-
-  dyngo (Constant _ a _) = Just (toDyn a)
-  dyngo (FunAp f e _) = do
-    f' <- dyngo f
-    e' <- dyngo e
-    dynApply f' e'
-  dyngo _ = Nothing
-
+    f' <- go f
+    e' <- go e
+    case f' `dynApp` e' of
+      Just x  -> Just x
+      -- this should never happen, as '$$' checks the application is
+      -- type-correct.
+      Nothing -> error ("can't apply function " ++ show f' ++ " to argument " ++ show e')
 
 -------------------------------------------------------------------------------
 -- Types
@@ -207,29 +165,8 @@ exprTypeArity :: Expr s m -> Int
 exprTypeArity = typeArity . exprTypeRep
 
 -- | Get the type of an expression.
-exprTypeRep :: Expr s m -> TypeRep
-exprTypeRep (Constant  _ _ ty) = ty
-exprTypeRep (Variable  _   ty) = ty
-exprTypeRep (FunAp     _ _ ty) = ty
-exprTypeRep (StateFun0 _ _ ty) = ty
-exprTypeRep (StateFun1 _ _ ty) = ty
-exprTypeRep (StateFun2 _ _ ty) = ty
-exprTypeRep (StateFun3 _ _ ty) = ty
-
--- | A 'TypeRep' for the state type. This is needed because, in
--- general, the state type may not be 'Typeable'.
-stateTypeRep :: TypeRep
-stateTypeRep = mkTyConApp stateTyCon []
-
--- | A 'TypeRep' for the monad type. This is needed because, in
--- general, the monad type may not be 'Typeable'.
-monadTypeRep :: TypeRep -> TypeRep
-monadTypeRep ty = mkTyConApp monadTyCon [ty]
-
--- | A 'TyCon' for the state type.
-stateTyCon :: TyCon
-stateTyCon = mkTyCon3 "" "" "state"
-
--- | A 'TyCon' for the monad type.
-monadTyCon :: TyCon
-monadTyCon = mkTyCon3 "" "" "monad"
+exprTypeRep :: Expr s m -> TypeRep s m
+exprTypeRep (Constant _ dyn)  = dynTypeRep dyn
+exprTypeRep (Variable _   ty) = ty
+exprTypeRep (FunAp    _ _ ty) = ty
+exprTypeRep StateVar          = stateTypeRep
--- Test/Spec/Type.hs
+++ Test/Spec/Type.hs
@@ -1,39 +1,164 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE MagicHash #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeOperators #-}
+
 -- |
 -- Module      : Test.Spec.Type
 -- Copyright   : (c) 2017 Michael Walker
 -- License     : MIT
 -- Maintainer  : Michael Walker <mike@barrucadu.co.uk>
 -- Stability   : experimental
--- Portability : portable
+-- Portability : AllowAmbiguousTypes, FlexibleInstances, GADTs, KindSignatures, MagicHash, MultiParamTypeClasses, ScopedTypeVariables, TypeApplications, TypeOperators
 --
--- Extra functions and utilities for dealing with types.
-module Test.Spec.Type where
+-- A reimplementation of Data.Typeable and Data.Dynamic, but twisted
+-- to my own malign purposes. This is basically all necessary because
+-- the constructor for
+-- @<http://hackage.haskell.org/package/base/docs/Data-Dynamic.html#t:Dynamic Data.Dynamic.Dynamic>@
+-- isn't exposed. On the other hand, if it was exposed it would be
+-- trivial to violate its invariant and write unsafeCoerce, swings and
+-- roundabouts...
+module Test.Spec.Type
+  ( -- * Dynamic
+    Dynamic
+  , toDyn
+  , fromDyn
+  , dynTypeRep
+  , dynApp
+
+  -- * Typeable
+  , HasTypeRep
+  , TypeRep
+  , (:~:)(..)
+  , rawTypeRep
+  , typeRep
+  , typeOf
+  , cast
+  , eqT
+  , gcast
+  , funResultTy
+  , typeArity
+  , stateTypeRep
+  ) where
 
 import Data.Proxy (Proxy(..))
-import Data.Typeable
+import Data.Typeable ((:~:)(..))
+import qualified Data.Typeable as T
+import GHC.Base (Any)
+import Unsafe.Coerce (unsafeCoerce)
 
 -------------------------------------------------------------------------------
--- * 'TyCon's
+-- Dynamic
 
--- | The function arrow.
-funTyCon :: TyCon
-funTyCon = tyCon (Proxy :: Proxy (() -> ()))
+-- | A dynamically-typed value, with state type @s@ and monad type
+-- @m@.
+data Dynamic (s :: *) (m :: * -> *) where
+  Dynamic :: Any -> TypeRep s m -> Dynamic s m
 
--- | The constructor of a type.
-tyCon :: Typeable a => proxy a -> TyCon
-tyCon = typeRepTyCon . typeRep
+instance Show (Dynamic s m) where
+  show d = "Dynamic <" ++ show (dynTypeRep d) ++ ">"
 
+-- | Convert a static value into a dynamic one.
+toDyn :: HasTypeRep s m a => a -> Dynamic s m
+toDyn a = Dynamic (unsafeCoerce a) (typeOf a)
+
+-- | Try to convert a dynamic value back into a static one.
+fromDyn :: forall s m a. HasTypeRep s m a => Dynamic s m -> Maybe a
+fromDyn (Dynamic x ty) = case unsafeCoerce x of
+  r | typeOf r == ty -> Just r
+    | otherwise -> Nothing
+
+-- | Get the type of a dynamic value.
+dynTypeRep :: Dynamic s m -> TypeRep s m
+dynTypeRep (Dynamic _ ty) = ty
+
+-- | Apply a dynamic function to a dynamic value, if the types match.
+dynApp :: Dynamic s m -> Dynamic s m -> Maybe (Dynamic s m)
+dynApp (Dynamic f t1) (Dynamic x t2) = case t1 `funResultTy` t2 of
+  Just t3 -> Just (Dynamic ((unsafeCoerce f) x) t3)
+  Nothing -> Nothing
 
 -------------------------------------------------------------------------------
--- * 'TypeRep's
+-- Typeable
+
+-- | Typeable, but which can represent a non-Typeable state and monad
+-- type.
+class HasTypeRep (s :: *) (m :: * -> *) (a :: *) where
+  typeRep# :: proxy a -> TypeRep s m
+
+instance {-# OVERLAPPABLE #-} T.Typeable a => HasTypeRep s m a where
+  typeRep# proxy = TypeRep (T.typeRep proxy)
+
+instance {-# OVERLAPPABLE #-} HasTypeRep s m a => HasTypeRep s m (m a) where
+  typeRep# _ = TypeRep $
+    let ty = (typeRep# :: proxy a -> TypeRep s m) Proxy
+    in T.mkTyConApp (T.mkTyCon3 "" "" ":monad:") [rawTypeRep ty]
+
+instance (HasTypeRep s m a, HasTypeRep s m b) => HasTypeRep s m (a -> b) where
+  typeRep# _ = TypeRep $
+    let tyA = (typeRep# :: proxy a -> TypeRep s m) Proxy
+        tyB = (typeRep# :: proxy b -> TypeRep s m) Proxy
+    in T.mkFunTy (rawTypeRep tyA) (rawTypeRep tyB)
+
+instance {-# INCOHERENT #-} HasTypeRep s m s where
+  typeRep# _ = stateTypeRep
+
+-- | A concrete representation of a type of some expression, with a
+-- state type @s@ and monad type @m@.
+data TypeRep (s :: *) (m :: * -> *) where
+  TypeRep :: T.TypeRep -> TypeRep s m
+  deriving Eq
+
+instance Show (TypeRep s m) where
+  show = show . rawTypeRep
+
+-- | The 'TypeRep' of the state variable.
+stateTypeRep :: TypeRep s m
+stateTypeRep = TypeRep $ T.mkTyConApp (T.mkTyCon3 "" "" ":state:") []
+
+-- | Get the underlying 'T.TypeRep' from a 'TypeRep'.
+rawTypeRep :: TypeRep s m -> T.TypeRep
+rawTypeRep (TypeRep ty) = ty
+
+-- | Takes a value of type @a@ with state @s@ in some monad @m@, and
+-- returns a concrete representation of that type.
+typeRep :: forall s m a proxy. HasTypeRep s m a => proxy a -> TypeRep s m
+typeRep = typeRep#
+
+-- | Get the type of a value.
+typeOf :: forall s m a. HasTypeRep s m a => a -> TypeRep s m
+typeOf _ = typeRep @s @m @a Proxy
+
+-- | The type-safe cast operation.
+cast :: forall s m a b. (HasTypeRep s m a, HasTypeRep s m b) => a -> Maybe b
+cast x = if typeRep @s @m @a Proxy == typeRep @s @m @b Proxy
+         then Just $ unsafeCoerce x
+         else Nothing
+
+-- | Extract a witness of equality of two types
+eqT :: forall s m a b. (HasTypeRep s m a, HasTypeRep s m b) => Maybe (a :~: b)
+eqT = if typeRep @s @m @a Proxy == typeRep @s @m @b Proxy
+      then Just $ unsafeCoerce Refl
+      else Nothing
+
+-- | Cast through a type constructor.
+gcast :: forall s m a b c. (HasTypeRep s m a, HasTypeRep s m b) => c a -> Maybe (c b)
+gcast x = fmap (\Refl -> x) (eqT @s @m @a @b)
+
+-- | Applies a type to a given function type, if the types match.
+funResultTy :: TypeRep s m -> TypeRep s m -> Maybe (TypeRep s m)
+funResultTy t1 t2 = TypeRep <$> rawTypeRep t1 `T.funResultTy` rawTypeRep t2
 
 -- | The arity of a type. Non-function types have an arity of 0.
-typeArity :: TypeRep -> Int
-typeArity ty = case splitTyConApp ty of
-  (con, [_, resultType]) | con == funTyCon -> 1 + typeArity resultType
-  _ -> 0
+typeArity :: TypeRep s m -> Int
+typeArity = go . rawTypeRep where
+  go ty = case T.splitTyConApp ty of
+    (con, [_, resultType]) | con == funTyCon -> 1 + go resultType
+    _ -> 0
 
--- | Apply a function type to a list of arguments.
-appFunTys :: TypeRep -> [TypeRep] -> TypeRep
-appFunTys f (ty:tys) = f `mkFunTy` appFunTys ty tys
-appFunTys f [] = f
+  funTyCon = T.typeRepTyCon (T.typeRep @Proxy @(() -> ()) Proxy)
