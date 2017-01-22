d529f8ee602769eafdcfdb77f4a0d8dec74b6880
Michael Walker <mike@barrucadu.co.uk>
2017-01-22 02:25:24 +0000

    Add a one-tier-at-a-time generator.

--- Test/Spec/Gen.hs
+++ Test/Spec/Gen.hs
@@ -28,11 +28,21 @@
 -- putMVar_int :state: x >>= \\_ -> takeMVar_int :state:
 -- putMVar_int :state: x >>= \\_ -> readMVar_int :state:
 -- @
-module Test.Spec.Gen where
+module Test.Spec.Gen
+  ( -- * Generating Terms
+    enumerate
 
-import Data.List (mapAccumL)
+  -- ** Controlled generation
+  , Generator
+  , newGenerator
+  , stepGenerator
+  , getTier
+  , mapTier
+  , maxTier
+  ) where
+
 import qualified Data.IntMap as M
-import Data.Maybe (catMaybes, isJust)
+import Data.Maybe (catMaybes, fromMaybe, isJust)
 import qualified Data.Set as S
 
 import Test.Spec.Expr
@@ -40,38 +50,52 @@ import Test.Spec.Type (unmonad)
 
 -- | Enumerate all well-typed expressions, in size order.
 enumerate :: [Expr s m] -> [[Expr s m]]
-enumerate baseTerms = snd (mapAccumL genTier initialTerms [1..]) where
-  -- generate a map of initial terms
-  initialTerms = termsList [M.fromList [(exprSize t, [t])] | t <- baseTerms]
+enumerate = tail . go 0 . newGenerator where
+  go tier g =
+    fromMaybe [] (getTier tier g) : go (tier+1) (stepGenerator g)
 
-  -- given a list of terms generated so far, and a size limit, combine
-  -- the terms to produce new ones which fit into the limit, emitting
-  -- all without free variables as the terms for this size-tier.
-  genTier tieredTerms size =
-    let newTiers = termsList [ tieredTerms
-                             , M.singleton size (genFunAps tieredTerms size)
-                             , M.singleton size (genBinds  tieredTerms size)
-                             , M.singleton size (genLets   tieredTerms size)
-                             ]
-        newTiers' = M.adjust (prune newTiers) size newTiers
-    in (newTiers', sizedTerms size newTiers')
+
+-------------------------------------------------------------------------------
+-- Controlled generation
+
+-- | A generator of expressions.
+data Generator s m = Generator { tiers :: M.IntMap [Expr s m], sofar :: Int }
+  deriving Eq
+
+-- | Create a new generator from a collection of basic expressions.
+newGenerator :: [Expr s m] -> Generator s m
+newGenerator baseTerms = Generator
+  { tiers = merge [M.fromList [(exprSize t, [t])] | t <- baseTerms]
+  , sofar = 0
+  }
+
+-- | Generate the next tier.
+stepGenerator :: Generator s m -> Generator s m
+stepGenerator g = Generator newTiers (sofar g + 1) where
+  newTiers =
+    let new = merge [ tiers g
+                    , M.singleton (sofar g + 1) funAps
+                    , M.singleton (sofar g + 1) binds
+                    , M.singleton (sofar g + 1) lets
+                    ]
+    in M.adjust (prune new) (sofar g + 1) new
 
   -- produce new terms by function application.
-  genFunAps = mkTerms $ \terms candidates ->
+  funAps = mkTerms $ \terms candidates ->
     [ t1 $$ t2 | t1 <- terms, exprTypeArity t1 > 0, t2 <- candidates]
 
   -- produce new terms by monad-binding variables.
-  genBinds = mkTerms $ \terms candidates ->
+  binds = mkTerms $ \terms candidates ->
     [bind var t1 t2 | t1 <- terms, isJust . unmonad $ exprTypeRep t1, t2 <- candidates, var <- "_" : map fst (freeVariables t2)]
 
   -- produce new terms by let-binding variables.
-  genLets = mkTerms $ \terms candidates ->
+  lets = mkTerms $ \terms candidates ->
     [let_ var t1 t2 | t1 <- terms, not (isVariable t1), t2 <- candidates, not (isVariable t2), (var,_) <- freeVariables t2]
 
   -- produce new terms
-  mkTerms f tieredTerms size = M.foldMapWithKey go tieredTerms where
+  mkTerms f = M.foldMapWithKey go (tiers g) where
     go tier terms =
-      let candidates = sizedTerms (size - tier - 1) tieredTerms
+      let candidates = sizedTerms (sofar g - tier) (tiers g)
       in catMaybes (f terms candidates)
 
   -- prune uninteresting expressions.
@@ -82,16 +106,38 @@ enumerate baseTerms = snd (mapAccumL genTier initialTerms [1..]) where
         in term' `notElem` sizedTerms (exprSize term') tieredTerms
       | otherwise = True
 
-  -- merge a list of maps of terms-by-size into a map of lists of
-  -- terms-by-size.
-  termsList = M.unionsWith (++)
-
   -- get all terms of the given size.
   sizedTerms = M.findWithDefault []
+
+-- | Get the terms of a given size, if they have been generated.
+getTier :: Int -> Generator s m -> Maybe [Expr s m]
+getTier tier g
+  -- this check is to avoid returning a partial result, which is
+  -- possible as the tiers are initially populated based on the base
+  -- terms.
+  | tier > sofar g = Nothing
+  | otherwise = M.lookup tier (tiers g)
+
+-- | Apply a function to the terms in a tier.
+--
+-- It is IMPORTANT that this function does not make any expressions
+-- larger or smaller! 'stepGenerator' assumes that every expression in
+-- a tier is of the correct size, and it WILL NOT behave properly if
+-- this invariant is broken!
+mapTier :: ([Expr s m] -> [Expr s m]) -> Int -> Generator s m -> Generator s m
+mapTier f tier g = g { tiers = M.adjust f tier (tiers g) }
+
+-- | Get the highest size generated so far.
+maxTier :: Generator s m -> Int
+maxTier = sofar
 
 
 -------------------------------------------------------------------------------
 -- Utilities
+
+-- | Merge a list of maps of lists.
+merge :: [M.IntMap [a]] -> M.IntMap [a]
+merge = M.unionsWith (++)
 
 -- | Remove duplicates from a list efficiently.
 ordNub :: Ord a => [a] -> [a]
