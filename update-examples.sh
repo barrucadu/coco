853c8958dd176a3f96f845caed46f33b8f01fbbb
Michael Walker <mike@barrucadu.co.uk>
2017-04-05 17:33:09 +0100

    Add a script to update example outputs.

---
+++ update-examples.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo -n "MVar:"
+time stack exec examples-mvar         > examples/MVar.out
+
+echo -ne "\nStack:"
+time stack exec examples-stack        > examples/Stack.out
+
+echo -ne "\nComplex Stack:"
+time stack exec examples-complexstack > examples/ComplexStack.out
